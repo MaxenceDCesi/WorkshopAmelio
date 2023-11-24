a3dfb879fe7e9ba027c2cd2349f992704c71776c
MaxenceDCesi <maxence.david@viacesi.fr>
2023-11-24 13:24:41 +0100

    test

--- WorkshopMain.cpp
+++ WorkshopMain.cpp
@@ -5,12 +5,10 @@ int main(void) {
 
     Objet2DChaine chain;
 
-    // Demander à l'utilisateur le nombre de carrés à ajouter
     int nombreDeCarres;
     cout << "Entrez le nombre de carres a ajouter : ";
     cin >> nombreDeCarres;
 
-    // Créer et ajouter les carrés à la liste
     for (int i = 0; i < nombreDeCarres; ++i) {
         int longueur;
         cout << "Entrez la longueur du carre " << i + 1 << " : ";
@@ -20,12 +18,10 @@ int main(void) {
         chain.ajouterEnQueue(carre);
     }
 
-    // Demander à l'utilisateur le nombre de rectangles à ajouter
     int nombreDeRectangles;
     cout << "Entrez le nombre de rectangles a ajouter : ";
     cin >> nombreDeRectangles;
 
-    // Créer et ajouter les rectangles à la liste
     for (int i = 0; i < nombreDeRectangles; ++i) {
         int largeur, hauteur;
         cout << "Entrez la largeur du rectangle " << i + 1 << " : ";
@@ -37,15 +33,12 @@ int main(void) {
         chain.ajouterEnQueue(rectangle);
     }
 
-    // Afficher les informations après l'ajout des carrés et rectangles
     cout << "Apres l'ajout des carres et rectangles :" << endl;
     chain.afficherInfos();
 
-    // Afficher le nombre de carrés et de rectangles dans la liste
     cout << "Nombre de carres dans la liste : " << chain.compterCarres() << endl;
     cout << "Nombre de rectangles dans la liste : " << chain.compterRectangles() << endl;
 
-    // Libérer la mémoire
     Objet2D* temp = chain.getHead();
     while (temp != nullptr) {
         Objet2D* next = temp->getSuivant();
